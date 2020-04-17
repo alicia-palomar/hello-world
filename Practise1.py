3c9e3c02dead854eb46a89e6dae73942b130822d
alicia-palomar <alicia.palomar@eu.medical.canon>
2020-04-17 16:16:43 +0200

    readjustment

--- Practise1.py
+++ Practise1.py
@@ -1,6 +1,17 @@
-np.load()
+import numpy as np
+import matplotlib.pyplot as plt
 
+patient_data = np.load("C:\Alicia\projects\hello-world\PatientData.npy")
 
-for k in [0, 5, 25, 50, 100, 150]:
+#plot some slices
+for k in [0, 50, 100, 150]:
     plt.figure()
     plt.imshow(patient_data[k], cmap=plt.cm.bone)
+
+#Histogram
+hist, bins = np.histogram(patient_data[50].flatten(), 256, [0, 256])
+plt.hist(patient_data[50].flatter(), 256, [0, 256])
+plt.show()
+
+
+
